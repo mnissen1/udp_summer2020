2975b8623207072e4f4638d3217078fd5d272288
mnissen1 <mnissen1@stanford.edu>
2020-10-08 10:15:31 -0700

    Updated full regression writeup and code

--- scripts_analysis/reg_nbr_traits.R
+++ scripts_analysis/reg_nbr_traits.R
@@ -1,38 +1,72 @@
----
-title: "Neighborhood Traits for SGC Regressions"
-author: "Matt Alvarez-Nissen"
-date: "8/15/2020"
-output: html_document
----
+# -------------------------------------------------------------------------
+# Created by: Matt Alvarez-Nissen                         
+# Date created: Aug. 15, 2020                
+# Last revised: Oct. 8, 2020                 
+# Project: UDP SGC     
+# Subproject: Full Regressions
+# Re: Clean and prepare ACS/Census data for full regressions
+# -------------------------------------------------------------------------
 
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-# Set working directory
-knitr::opts_knit$set(root.dir = rprojroot::find_rstudio_root_file())
-```
+# Script Description ------------------------------------------------------
 
-```{r}
+# This script takes all raw (tract-level) ACS/Census data, crosswalks data with 
+# 2000 boundaries, and produces a cleaned master dataset to use in the full
+# regressions write-up.
+
+# Inputs:
+# Various years of ACS/Census data
+# LTBD
+
+# Outputs:
+# Cleaned and crosswalked master ACS/Census datatable
+
+# Update log: 
+# 10/8/20 - converted from RMarkdown to R script for faster processing.
+
+# Setup -------------------------------------------------------------------
+
+# Packages: 
 library(tidyverse)
 
-# Files
-census_tract_00_file <- "./SGC/census_tract_00.csv"
-acs_tract_09_file <- "./SGC/acs_tract_09.csv"
-acs_tract_10_file <- "./SGC/acs_tract_10.csv"
-acs_tract_13_file <- "./SGC/acs_tract_13.csv"
-acs_tract_16_file <- "./SGC/acs_tract_16.csv"
-acs_tract_18_file <- "./SGC/acs_tract_18.csv"
+# Directories: 
+homedir <- "E:/udp_summer2020/"
+workdir <- "sgc_data/raw/"
+savedir <- "sgc_data/cleaned/"
+setwd(homedir)
 
-crosswalk_00_10_file <- "./SGC/crosswalk_2000_2010.csv"
-```
+# Import data:
+# Census/ACS data
+census_tract_00 <- read_csv(paste0(homedir, workdir, "census_tract_00.csv"))
+acs_tract_09 <- read_csv(paste0(homedir, workdir, "acs_tract_09.csv"))
+acs_tract_10 <- read_csv(paste0(homedir, workdir, "acs_tract_10.csv"))
+acs_tract_13 <- read_csv(paste0(homedir, workdir, "acs_tract_13.csv"))
+acs_tract_16 <- read_csv(paste0(homedir, workdir, "acs_tract_16.csv"))
+acs_tract_18 <- read_csv(paste0(homedir, workdir, "acs_tract_18.csv"))
+
+# LTBD crosswalking file
+## Brown University Diversity and Disparities
+## https://s4.ad.brown.edu/projects/diversity/Researcher/LTBDDload/DataList.aspx
+crosswalk_00_10 <- read_csv(paste0(homedir, workdir, "crosswalk_2000_2010.csv"))
+
+# Parameters:
+# Filter crosswalk to relevant tract IDs
+# Create list of FIPS from ACS data to filter on
+fips_00_filter <-
+  paste(census_tract_00$stctytrct, collapse = "|")
+
+fips_09_filter <-
+  paste(acs_tract_09$stctytrct, collapse = "|")
+
+# Main Script ------------------------------------------------------------------
 
 # Read in data
-```{r}
-# 2000
+
+# 2000 Census
 ## Can't use percentages, need to crosswalk first
 census_tract_00 <- 
-  read_csv(census_tract_00_file) %>% 
+  census_tract_00 %>% 
   select(
-    GEOID = stctytrct,
+    stctytrct,
     pop_tot,
     pop_whi_nonhis,
     pop_hisp,
@@ -52,12 +86,12 @@ census_tract_00 <-
     .cols = pop_tot:pop_college
   )
 
-# 2009
+# 2009 ACS
 ## Can't use percentages, need to crosswalk first
 acs_tract_09 <- 
-  read_csv(acs_tract_09_file) %>% 
+  acs_tract_09 %>% 
   select(
-    GEOID = stctytrct,
+    stctytrct,
     pop_tot,
     pop_whi_nonhis,
     pop_hisp,
@@ -77,9 +111,9 @@ acs_tract_09 <-
     .cols = pop_tot:pop_college
   )
 
-# 2010
+# 2010 ACS
 acs_tract_10 <- 
-  read_csv(acs_tract_10_file) %>% 
+  acs_tract_10 %>% 
   select(
     GEOID = stctytrct,
     pop_tot,
@@ -97,9 +131,9 @@ acs_tract_10 <-
     .cols = pop_tot:pct_college
   )
 
-# 2013
+# 2013 ACS
 acs_tract_13 <- 
-  read_csv(acs_tract_13_file) %>% 
+  acs_tract_13 %>% 
   select(
     GEOID = stctytrct,
     pop_tot,
@@ -117,9 +151,9 @@ acs_tract_13 <-
     .cols = pop_tot:pct_college
   )
 
-# 2016
+# 2016 ACS
 acs_tract_16 <- 
-  read_csv(acs_tract_16_file) %>% 
+  acs_tract_16 %>% 
   select(
     GEOID = stctytrct,
     pop_tot,
@@ -137,9 +171,9 @@ acs_tract_16 <-
     .cols = pop_tot:pct_college
   )
 
-# 2018
+# 2018 ACS
 acs_tract_18 <- 
-  read_csv(acs_tract_10_file) %>% 
+  acs_tract_18 %>% 
   select(
     GEOID = stctytrct,
     pop_tot,
@@ -156,25 +190,8 @@ acs_tract_18 <-
     ~ paste0(., "_18"),
     .cols = pop_tot:pct_college
   )
-```
 
 # Crosswalk 2000 to 2010 Census boundaries
-```{r read-crosswalk-file, echo=FALSE, warning=FALSE, message=FALSE}
-## Read in crosswalk file
-crosswalk_00_10 <- read_csv(crosswalk_00_10_file)
-```
-
-```{r filter-crosswalk, echo=FALSE, warning=FALSE, message=FALSE}
-## Filter crosswalk to relevant tract IDs
-### Use crosswalk filter from Brown University Diversity and Disparities
-### https://s4.ad.brown.edu/projects/diversity/Researcher/LTBDDload/DataList.aspx
-
-# Create list of FIPS from ACS data to filter on
-fips_00_filter <-
-  paste(census_tract_00$GEOID, collapse = "|")
-
-fips_09_filter <-
-  paste(acs_tract_09$GEOID, collapse = "|")
 
 # Filter crosswalk data by relevant FIPS
 crosswalk_filter_00 <-
@@ -184,17 +201,16 @@ crosswalk_filter_00 <-
 crosswalk_filter_09 <-
   crosswalk_00_10 %>% 
   filter(str_detect(trtid00, fips_09_filter))
-```
 
-## 2000 crosswalk
-```{r do-crosswalk, echo=FALSE, warning=FALSE, message=FALSE}
+# Remove original crosswalk
+rm(crosswalk_00_10)
+
+# 2000 crosswalk
 ## Interpolate 2000 boundary data to 2010 tracts
 census_00_xwalk <-
   census_tract_00 %>% 
-  # rename full FIPS code for a join
-  rename(trtid00 = GEOID) %>% 
   # join to filtered crosswalk
-  full_join(crosswalk_filter_00, by = "trtid00") %>% 
+  full_join(crosswalk_filter_00, by = c("stctytrct" = "trtid00")) %>% 
   # change weight from character to numeric type
   mutate(weight = as.numeric(weight)) %>% 
   # multiply all data by its weight
@@ -212,7 +228,7 @@ census_00_xwalk <-
   group_by(trtid10) %>% 
   # sum up all variables
   summarise_all(sum) %>% 
-  # rename trtid10 to GEOID
+  # rename trtid10 to stctytrct
   rename(GEOID = trtid10) %>% 
   # find percentages
   transmute(
@@ -227,17 +243,16 @@ census_00_xwalk <-
     pov_rate_00 = pop_pov_pov_00 / pop_pov_tot_00,
     pct_college_00 = pop_college_00 / pop_25over_00
   )
-```
 
-## 2000 crosswalk
-```{r do-crosswalk, echo=FALSE, warning=FALSE, message=FALSE}
+# remove original 2000 data
+rm(census_tract_00)
+
+# 2009 crosswalk
 ## Interpolate 2000 boundary data to 2010 tracts
 acs_09_xwalk <-
   acs_tract_09 %>% 
-  # rename full FIPS code for a join
-  rename(trtid00 = GEOID) %>% 
   # join to filtered crosswalk
-  full_join(crosswalk_filter_09, by = "trtid00") %>% 
+  full_join(crosswalk_filter_09, by = c("stctytrct" = "trtid00")) %>% 
   # change weight from character to numeric type
   mutate(weight = as.numeric(weight)) %>% 
   # multiply all data by its weight
@@ -270,16 +285,28 @@ acs_09_xwalk <-
     pov_rate_09 = pop_pov_pov_09 / pop_pov_tot_09,
     pct_college_09 = pop_college_09 / pop_25over_09
   )
-```
+
+# remove original 2009 data
+rm(acs_tract_09)
 
 # Combine Census and ACS data to single df, write to CSV
-```{r}
-census_00_xwalk %>% 
+census_merged <-
+  census_00_xwalk %>% 
   full_join(acs_09_xwalk, by = "GEOID") %>% 
   full_join(acs_tract_10, by = "GEOID") %>% 
   full_join(acs_tract_13, by = "GEOID") %>%
   full_join(acs_tract_16, by = "GEOID") %>%
-  full_join(acs_tract_18, by = "GEOID") %>% 
-  write_csv(path = "./SGC/tract characteristics/all_years_tract_CA.csv")
-```
+  full_join(acs_tract_18, by = "GEOID") 
 
+# remove old data
+rm(
+  census_00_xwalk, acs_09_xwalk, acs_tract_10, acs_tract_13, 
+  acs_tract_16, acs_tract_18, crosswalk_filter_00, crosswalk_filter_09
+)
+
+# Save Results ------------------------------------------------------------
+# write merged census data
+write_csv(census_merged, paste0(homedir, savedir, "all_years_tract_CA.csv"))
+
+# Clean up environment
+rm(list = ls())
